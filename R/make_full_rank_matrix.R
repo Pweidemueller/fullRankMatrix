72989d8940b9ecac71dbec0cc0676eafe9937911
Paula Weidemueller <pweide@ebi.ac.uk>
2022-04-24 23:21:35 +0100

    add full function

--- R/make_full_rank_matrix.R
+++ R/make_full_rank_matrix.R
@@ -1,6 +1,9 @@
-#' Create a full rank matrix. The function first removes empty columns. The it discovers linear dependent columns, then renames and removes columns to make the matrix full rank.
+#' Create a full rank matrix
+#'
+#' This function first removes empty columns. Then it discovers linear dependent columns, then renames and removes columns to make the matrix full rank.
 #'
 #' @param mat A matrix.
+#' @param verbose Print how column numbers change with each operation.
 #'
 #' @return A matrix of full rank.
 #' @export
@@ -10,21 +13,40 @@
 #' c2 <- 1-c1
 #' c3 <- integer(10)
 #' c4 <- c1
-#' mat <- as.matrix(data.frame(c1, c2, c3, c4))
+#' c5 <- 2*c2
+#' mat <- as.matrix(data.frame(c1, c2, c3, c4, c5))
 #' make_full_rank_matrix(mat)
+#' mat_full <- make_full_rank_matrix(mat, verbose=TRUE)
 
-make_full_rank_matrix <- function(mat){
-  mat_mod <- remove_empty_columns(mat)
-  mat_mod <- merge_duplicated(mat_mod)
+make_full_rank_matrix <- function(mat, verbose=FALSE){
+  if (verbose){
+    print(sprintf("The original matrix contains %i rows and %i columns. The matrix has rank %i.",
+                  dim(mat)[1], dim(mat)[2], qr(mat)$rank))
+  }
+  mat_mod <- remove_empty_columns(mat, verbose=verbose)
+  mat_mod <- merge_duplicated(mat_mod, verbose=verbose)
+  mat_mod <- find_lindependent_coef(mat_mod, verbose=verbose)
+  if (ncol(mat_mod) > qr(mat)$rank){
+    stop(print("The modified matrix still has more columns than implied by rank. Check manually why modified matrix is not full rank after applying make_full_rank_matrix()."))
+  }
+  if (verbose){
+    print("The matrix is full rank now.")
+  }
   return(mat_mod)
 }
 
-remove_empty_columns <- function(mat) {
+remove_empty_columns <- function(mat, verbose=FALSE) {
   emptry_col <- apply(mat, MARGIN = 2, FUN = function(x) {all(x==0)})
-  return(mat[, !emptry_col])
+  mat_red <- mat[, !emptry_col]
+  if (verbose){
+    print(sprintf("The matrix after removing empty columns contains %i rows and %i columns",
+                  dim(mat_red)[1], dim(mat_red)[2]))
+
+  }
+  return(mat_red)
 }
 
-merge_duplicated <- function(mat) {
+merge_duplicated <- function(mat, verbose=FALSE) {
   if (is.matrix(mat)==FALSE){
     stop(print("Input matrix has to be of type matrix. If your matrix is stored as a dataframe, convert like so: `mat <- as.matrix(mat)`."))
   }
@@ -43,5 +65,35 @@ merge_duplicated <- function(mat) {
     colnames_unique[keep] <- paste0("(", colnames_unique[keep], "_AND_", colnames_duplicated[c], ")")
   }
   colnames(mat_unique) <- colnames_unique
+  if (verbose){
+    print(sprintf("The matrix after merging duplicate columns contains %i rows and %i columns.",
+                  dim(mat_unique)[1], dim(mat_unique)[2]))
+  }
   return(mat_unique)
+}
+
+find_lindependent_coef <- function(mat, verbose=FALSE) {
+  if (is.matrix(mat)==FALSE){
+    stop(print("Input matrix has to be of type matrix. If your matrix is stored as a dataframe, convert like so: `mat <- as.matrix(mat)`."))
+  }
+  linear_combs <- caret::findLinearCombos(mat)[c("linearCombos", "remove")]
+  colnames_matr <- colnames(mat)
+  new_colnames <- colnames_matr
+  for (ncombo in seq_len(length(linear_combs$linearCombos))){
+    combo <- linear_combs$linearCombos[[ncombo]]
+    remove <- linear_combs$remove[[ncombo]]
+    keep <- combo[combo!=remove]
+    for (keep_col in keep){
+      add_names <- c(colnames_matr[remove], colnames_matr[keep[keep!=keep_col]])
+      add_names <- paste(add_names, collapse="_COMB_")
+      new_colnames[keep_col] = paste0(new_colnames[keep_col], "_OR_(", add_names, ")")
+    }
+  }
+  colnames(mat) <- new_colnames
+  mat_red <- mat[, -linear_combs$remove]
+  if (verbose){
+    print(sprintf("The matrix after finding linearly dependent columns contains %i rows and %i columns.",
+                  dim(mat_red)[1], dim(mat_red)[2]))
+  }
+  return(mat_red)
 }
--- tests/testthat/test-make_full_rank_matrix.R
+++ tests/testthat/test-make_full_rank_matrix.R
@@ -21,3 +21,52 @@ test_that("merging duplicated columns as expected", {
   mat2 <- merge_duplicated(mat)
   expect_equal(mat2, mat_dedupl)
 })
+
+test_that("remove linear dependent columns as expected", {
+  set.seed(1000)
+  c1 <- rbinom(10, 1, .4)
+  c2 <- 1-c1
+  c3 <- integer(10)
+  c4 <- c1
+  mat <- as.matrix(data.frame(c1, c2, c3, c4))
+  mat_linind <- as.matrix(data.frame(c1, c2))
+  colnames(mat_linind) <- c('c1_OR_(c4)', 'c2')
+  mat2 <- find_lindependent_coef(mat)
+  expect_equal(mat2, mat_linind)
+})
+
+test_that("make full rank column as expected", {
+  c1 <- rbinom(10, 1, .4)
+  c2 <- 1-c1
+  c3 <- rnorm(10)
+  c4 <- 10*c3
+  c5 <- c1
+  c6 <- integer(10)
+  c7 <- rep(1,10)
+  mat <- as.matrix(data.frame(c1, c2, c3, c4, c5, c6, c7))
+
+  mat_fullrank <- as.matrix(data.frame(c1, c2, c3))
+  colnames(mat_fullrank) <- c("(c1_AND_c5)_OR_(c7_COMB_c2)", "c2_OR_(c7_COMB_(c1_AND_c5))", "c3_OR_(c4)")
+  mat2 <- make_full_rank_matrix(mat)
+  expect_equal(mat2, mat_fullrank)
+})
+
+test_that("merge_duplicated() errors if input is not of type matrix", {
+  set.seed(1000)
+  c1 <- rbinom(10, 1, .4)
+  c2 <- 1-c1
+  c3 <- integer(10)
+  c4 <- c1
+  mat <- data.frame(c1, c2, c3, c4)
+  expect_error(merge_duplicated(mat))
+})
+
+test_that("find_lindependent_coef() errors if input is not of type matrix", {
+  set.seed(1000)
+  c1 <- rbinom(10, 1, .4)
+  c2 <- 1-c1
+  c3 <- integer(10)
+  c4 <- c1
+  mat <- data.frame(c1, c2, c3, c4)
+  expect_error(find_lindependent_coef(mat))
+})
